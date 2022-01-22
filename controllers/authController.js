22f2822901d4d246db2be9360005687ebefb546a
Aman23Gupta <amang234284@gmail.com>
2022-01-22 20:57:59 +0530

    forgot password + nodemailer

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,6 +1,7 @@
 const { promisify } = require('util');
 const jwt = require('jsonwebtoken');
 const User = require('../models/userModel');
+const sendEmail = require('../utils/email');
 
 const signToken = (id) =>
   jwt.sign({ id }, process.env.JWT_SECRET, {
@@ -39,14 +40,14 @@ exports.login = async (req, res, next) => {
 
     //1) Check if email and password exist
     if (!email || !password) {
-      return next(new Error('Please provide email and password'));
+      throw new Error('Please provide email and password');
     }
 
     //2) check if user exist and password is correct
     const user = await User.findOne({ email }).select('+password');
 
     if (!user || !(await user.correctPassword(password, user.password))) {
-      return next(new Error('incorrect email or password'));
+      throw new Error('incorrect email or password');
     }
 
     //3) everything is ok, send token to client
@@ -58,53 +59,93 @@ exports.login = async (req, res, next) => {
   } catch (err) {
     res.status(400).json({
       status: 'fail',
-      message: err,
+      message: err.message,
     });
   }
 };
 
 exports.protect = async (req, res, next) => {
-  //1) get token and check if it's exist
-  let token;
-  if (
-    req.headers.authorization &&
-    req.headers.authorization.startsWith('Bearer')
-  ) {
-    token = req.headers.authorization.split(' ')[1];
-  }
-  if (!token) {
-    return next(
-      new Error('You are not logged in! Please log in to get access.')
-    );
-  }
-
-  //2) varifiaction token
-  let decoded;
   try {
-    decoded = await promisify(jwt.verify)(token, process.env.JWT_SECRET);
+    //1) get token and check if it's exist
+    let token;
+    if (
+      req.headers.authorization &&
+      req.headers.authorization.startsWith('Bearer')
+    ) {
+      token = req.headers.authorization.split(' ')[1];
+    }
+    if (!token) {
+      throw new Error('You are not logged in! Please log in to get access.');
+    }
+
+    //2) varifiaction token
+    const decoded = await promisify(jwt.verify)(token, process.env.JWT_SECRET);
+
+    //3) check if user still exist
+    const currentUser = await User.findById(decoded.id);
+    if (!currentUser) {
+      throw new Error('The user belonging to this token does no longer exist.');
+    }
+
+    //4) check if user changed password after that token was issued
+    if (currentUser.changedPasswordAfter(decoded.iat)) {
+      throw new Error('User recently changed password! Please log in again.');
+    }
+
+    // Grant access
+    req.user = currentUser;
+    next();
   } catch (err) {
-    res.status(401).json({
+    res.status(400).json({
       status: 'fail',
-      message: err,
+      message: err.message,
     });
   }
-
-  //3) check if user still exist
-  const currentUser = await User.findById(decoded.id);
-  if (!currentUser) {
-    return next(
-      new Error('The user belonging to this token does no longer exist.')
-    );
-  }
-
-  //4) check if user changed password after that token was issued
-  if (currentUser.changedPasswordAfter(decoded.iat)) {
-    return next(
-      new Error('User recently changed password! Please log in again.')
-    );
-  }
-
-  // Grant access
-  req.user = currentUser;
-  next();
 };
+
+exports.forgotPassword = async (req, res, next) => {
+  try {
+    //1) get user based on pasted email
+    const user = await User.findOne({ email: req.body.email });
+    if (!user) {
+      throw new Error('there is now user with this email');
+    }
+
+    //2) generate the random reset token
+    const resetToken = user.createPasswordResetToken();
+    await user.save({ validateBeforeSave: false });
+
+    //3) send it to user's email
+    const resetURL = `${req.protocol}://${req.get(
+      'host'
+    )}/api/v1/users/resetPassword/${resetToken}`;
+
+    const message = `Forgot your password? Submit a PATCH request with your new password and passwordConfirm to: 
+    ${resetURL}.\nIf you didn't forget your password, please ignore this email!`;
+
+    try {
+      await sendEmail({
+        email: user.email,
+        subject: 'Your password reset token (valid for 10 min)',
+        message,
+      });
+
+      res.status(200).json({
+        status: 'success',
+        message: 'Token sent to email!',
+      });
+    } catch (err) {
+      user.passwordResetToken = undefined;
+      user.passwordResetExpires = undefined;
+      await user.save({ validateBeforeSave: false });
+      throw new Error('There was an error sending the email. Try again later!');
+    }
+  } catch (err) {
+    res.status(400).json({
+      status: 'fail',
+      message: err.message,
+    });
+  }
+};
+
+exports.resetPassword = () => {};
--- models/userModel.js
+++ models/userModel.js
@@ -1,3 +1,4 @@
+const crypto = require('crypto');
 const mongoose = require('mongoose');
 const validator = require('validator');
 const bcrypt = require('bcryptjs');
@@ -34,6 +35,8 @@ const userSchema = new mongoose.Schema({
     },
   },
   passwordChangedAt: Date,
+  passwordResetToken: String,
+  passwordResetExpires: Date,
 });
 
 userSchema.pre('save', async function (next) {
@@ -52,16 +55,31 @@ userSchema.methods.correctPassword = async function (
 };
 
 userSchema.methods.changedPasswordAfter = function (JWTTimestamp) {
-  if(this.passwordChangedAt) {
+  if (this.passwordChangedAt) {
     const changedTimestamp = parseInt(
       this.passwordChangedAt.getTime() / 1000,
       10
     );
-    
+
     return JWTTimestamp < changedTimestamp;
   }
   return false;
-}
+};
+
+userSchema.methods.createPasswordResetToken = function () {
+  const resetToken = crypto.randomBytes(32).toString('hex');
+
+  this.passwordResetToken = crypto
+    .createHash('sha256')
+    .update(resetToken)
+    .digest('hex');
+
+  console.log({ resetToken }, this.passwordResetToken);
+
+  this.passwordResetExpires = Date.now() + 10 * 60 * 1000;
+
+  return resetToken;
+};
 
 const User = mongoose.model('User', userSchema);
 
